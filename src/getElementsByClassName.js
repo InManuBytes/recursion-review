762b822be9b5721f690ec5671d81eb9c1bfdc3ae
RomanEmmons <roman.emmons@gmail.com>
2019-07-25 20:54:53 -0700

    Solution

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  let elementArray = [];
+  //console.log("DOC BODY CLASSLIST", document.body.classList);
+  function recurse(element) {
+    console.log("current element: ", element, typeof element);
+    //if (element.classList === undefined) {
+    console.log("I'm inside the undefined");
+    //return; //will a break statement work?
+    //} else
+    if (element.classList.contains(className)) {
+      elementArray.push(element);
+    }
+    if (element.childNodes.length) {
+      for (let i = 0; i < element.childNodes.length; i++) {
+        recurse(element.childNodes[i]);
+      }
+    }
+  }
+  // console.log("DOC BODY", document.body.childNodes);
+  recurse(document.body);
+  return elementArray;
 };
