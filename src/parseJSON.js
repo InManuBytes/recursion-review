a2539ba5061ba9750b144e53d72ac339f1bd8481
RomanEmmons <roman.emmons@gmail.com>
2019-07-27 16:05:59 -0700

    Brainstorm for parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,13 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  //if json is unparsable then throw syntax error
+  for(let i =1; i< json.length;i++)
+
+  let arrayExp = /^\'\[/;
+  //test if array or objects with regular expressions
+  //not all double quotes need to be removed
+  '{"foo": "bar"}' -> obj =   {foo: "bar"}
+
+
 };
