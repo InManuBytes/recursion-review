5b7236cee2508d7040f285e07605f98e0d21d02f
RomanEmmons <roman.emmons@gmail.com>
2019-07-27 15:28:48 -0700

    Complete BMR without helper function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,24 +4,29 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className) {
+var getElementsByClassName = function(className, element) {
+  //console.log("current element: ", element, typeof element);
+  //sets the first node to be the document's body
+  //can you assign the value of the second argument that is undefined within
+  //the function?
   let elementArray = [];
-  //console.log("DOC BODY CLASSLIST", document.body.classList);
-  function recurse(element) {
-    console.log("current element: ", element, typeof element);
-    if (element.classList === undefined) {
-      console.log("I'm inside the undefined");
-      return; //will a break statement work?
-    } else if (element.classList.contains(className)) {
+  if (element === undefined) {
+    element = document.body;
+  }
+
+  if (element.classList) {
+    if (element.classList.contains(className)) {
       elementArray.push(element);
     }
-    if (element.childNodes.length) {
-      for (let i = 0; i < element.childNodes.length; i++) {
-        recurse(element.childNodes[i]);
-      }
+  }
+
+  if (element.childNodes.length) {
+    for (let i = 0; i < element.childNodes.length; i++) {
+      elementArray = elementArray.concat(
+        getElementsByClassName(className, element.childNodes[i])
+      );
     }
   }
-  // console.log("DOC BODY", document.body.childNodes);
-  recurse(document.body);
+  // recurse(document.body);
   return elementArray;
 };
